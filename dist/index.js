62d4bf7d39d7e9dbbf3d1a4b23eb773c8cc8f542
marcinzygan <soul@o2.pl>
2023-03-15 18:36:40 +0100

    readonly , public .private

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,25 @@
 "use strict";
 // TYPESCRIPT LESSONS
 // CLASSES
+// class User {
+//   email: string;
+//   name: string;
+//   private readonly city: string = "Kapop";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//     console.log(this.city);
+//   }
+// }
+// const marcin = new User("soul@22.op", "soul");
+// console.log(marcin.email);
+// console.log(marcin.city); cannot access city beecause its privete it can only be accessed inside of the class
+// ANOTHER WAY OF WRITING THE CLASS
 class User {
-    constructor(email, name) {
+    constructor(email) {
         this.email = email;
-        this.name = name;
+        this.city = "Kopop";
     }
 }
-const marcin = new User("soul@22.op", "soul");
+const marcin = new User("soul@op.pp");
 console.log(marcin.email);
--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,28 @@
 
 // CLASSES
 
+// class User {
+//   email: string;
+//   name: string;
+//   private readonly city: string = "Kapop";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//     console.log(this.city);
+//   }
+// }
+
+// const marcin = new User("soul@22.op", "soul");
+
+// console.log(marcin.email);
+// console.log(marcin.city); cannot access city beecause its privete it can only be accessed inside of the class
+
+// ANOTHER WAY OF WRITING THE CLASS
 class User {
-  email: string;
-  name: string;
-  constructor(email: string, name: string) {
-    this.email = email;
-    this.name = name;
-  }
+  readonly city = "Kopop";
+  constructor(public email: string) {}
 }
 
-const marcin = new User("soul@22.op", "soul");
+const marcin = new User("soul@op.pp");
 
 console.log(marcin.email);
